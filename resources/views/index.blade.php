70668e473883ebad3de3a24baa3487db1f0107be
RitonX <RitonX@dev.github.com>
2022-01-25 22:39:37 +0330

    Create Project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur atque aut, deserunt distinctio dolor ea eius, error ex id iste iusto odit quo tempore unde velit veritatis vero voluptatum!
